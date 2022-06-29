adef8ef9c6a815736f78bf6f719cedf70577bb20
Victor Veloso <vitovls37@gmail.com>
2022-06-29 17:28:30 -0300

    feat: Implementando todas as alterações na home page e fazendo primeira chamada da API

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react";
+import { ThemeProvider } from "styled-components";
+import MyHead from "../components/MyHead";
+import ProductList from "../components/ProductList";
+import GlobalStyle from "../styles/Global.styled";
+import theme from "../styles/theme";
+
 export default function Home() {
+
+  const [products, setProducts] = useState({
+    items: [],
+  });
+
+  const [pagination, setPagination] = useState({
+    currentPage: 1,
+    totalPages: 1,
+  })
+
+  useEffect(() => {
+    fetch(`https://wine-back-test.herokuapp.com/products?page=${pagination.currentPage}&limit=10`)
+      .then(res => res.json())
+      .then((data) => {
+        setProducts(data);
+        setPagination({
+          currentPage: data.page,
+          totalPages: data.totalPages,
+        });
+      });
+  }, [products])
+
   return (
-    <div>
-      <h1>Hello Winer</h1>
-    </div>
+    <>
+      <MyHead /> 
+      <ThemeProvider theme={theme}>
+      <GlobalStyle />
+        <ProductList products={products.items} />
+      </ThemeProvider>
+    </>
   );
 }
