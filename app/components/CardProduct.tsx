864840428a6aedea0af7442570a9b144f19dbcdd
Victor Veloso <vitovls37@gmail.com>
2022-06-29 20:15:17 -0300

    style: Adicionando estilização nos cards de produtos

--- app/components/CardProduct.tsx
+++ app/components/CardProduct.tsx
@@ -4,38 +4,108 @@ import styled from "styled-components";
 import ITheme from "../interfaces/ITheme";
 
 const StyledCardProduct = styled.section`
-background-color: ${props => props.theme.colors.white};
-border-radius: 5px;	
-box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-padding: 20px;
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-
-.product-price {
-  color: ${props => props.theme.colors.grey};
-  text-decoration: line-through;
-  margin-bottom: 10px;
+.product-container {
+  background-color: ${props => props.theme.colors.white};
+  border-radius: 5px;	
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 
-.product-discount {
-  background-color: ${props => props.theme.colors.tertiary};
+.product-title {
+  font-size: 16px;
+  text-align: center;
+  margin: 10px 0 10px 0;
+}
+
+.product-price {
+  .product-original-price {
+    color: ${props => props.theme.colors.grey};
+    text-decoration: line-through;
+    margin: 20px 15px 20px 30px;
+    font-size: 11px;
+  }
+  
+  .product-discount-porcetage {
+    background-color: ${props => props.theme.colors.tertiary};
+    color: ${props => props.theme.colors.white};
+    padding: 2px 5px;
+    font-size: 11px;
+  }
+  
+  .product-price-member-container {
+    display: flex;
+    align-items: center;
+    font-size: 11px;
+    padding: 10px 5px;
+    weight: bold;
+
+    .product-price-member {
+      padding: 0 5px;
+      font-size: 11px;
+      color: ${props => props.theme.colors.quaternary};
+      
+      strong {
+        font-size: 23px;
+      }
+    }
+
+    .product-text-member {
+      padding: 0 5px;
+    }
+  }
+
+  .product-price-no-member-container {
+    font-size: 11px;
+    text-align: center;
+    color: ${props => props.theme.colors.grey};
+  }
+}
+
+button {
+  background-color: ${props => props.theme.colors.secondary};
+  border: none;
+  border-radius: 5px;
   color: ${props => props.theme.colors.white};
-  padding: 5px 10px;
+  padding: 10px;
+  font-size: 16px;
+  font-weight: bold;
+  margin-top: 10px;
+  cursor: pointer;
+  width: 100%;
+}
 `;
 
 export default function CardProduct({ product }: { product: IProduct }) {
   return (
-    <StyledCardProduct>
-        <Image width="150px" height="150px" src={product.image} />
-        <h3>{product.name}</h3>
-        <section>
-          <span className="product-price">R${product.price}</span>
-          <span className="product-discount">{product.discount}% OFF</span>
-          <div></div>
-          <div>{product.priceMember}</div>
+    <>
+      <StyledCardProduct>
+        <section className="product-container">
+          <Image width="200px" height="180px" src={product.image} />
+          <h3 className="product-title">{product.name}</h3>
+          <section className="product-price">
+            <span className="product-original-price">R${product.price}</span>
+            <span className="product-discount-porcetage">{product.discount}% OFF</span>
+            <section className="product-price-member-container">
+              <h3 className="product-text-member">
+                SÓCIO WINE
+              </h3>
+              <p className="product-price-member">
+                R$
+                <strong>{product.priceMember.toFixed(2).split(".")[0]}</strong>
+                ,{product.priceMember.toFixed(2).split(".")[1]}
+              </p>
+            </section>
+            <section className="product-price-no-member-container">
+                NÃO SÓCIO R$ {product.priceNonMember}
+            </section>
+          </section>
         </section>
-    </StyledCardProduct>
+        <button>Adicionar</button>
+      </StyledCardProduct>
+    </>
   );
 }
