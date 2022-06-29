e50c27f3be6e137037d95453d05df12d6087dc5d
Victor Veloso <vitovls37@gmail.com>
2022-06-29 17:13:17 -0300

    feat: Criação do Head com importações das fontes

---
+++ app/components/MyHead.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function MyHead() {
+  return (
+    <Head>
+      <title>Winer</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link href="/Neo Sans Std Regular.otf" rel="stylesheet" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+      <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet"/>
+    </Head>
+  );
+}
