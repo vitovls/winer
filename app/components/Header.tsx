cf0fb65feb85677fe7a1deca05967ff5c69deccd
Victor Veloso <vitovls37@gmail.com>
2022-06-30 00:51:06 -0300

    style/refac: Nova estilização e refatoração do Header e adição da quantidade de itens do carrinho

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,76 +1,96 @@
 import Image from "next/image";
 import styled from "styled-components";
+import { IProduct } from "../interfaces/IProducts";
 
 const HeaderStyled = styled.header`
   display: flex;
-  justify-content: space-between;
   align-items: center;
+  justify-content: space-between;
   padding: 20px;
   background-color: ${props => props.theme.colors.white};
+  border-bottom: 1px solid ${props => props.theme.colors.grey};
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
   height: 80px;
+  overflow: hidden;
 
   a {
     cursor: pointer;
   }
-  
-  .selected-link {
-    color: ${props => props.theme.colors.primary};
-    border-bottom: 2px solid ${props => props.theme.colors.primary};
-  }
+
 
   .links-header {
+    height: 100%;
+    width: 70%;
     display: flex;
-    justify-content: space-between;
-    padding: 20px;
-    width: 500px;
-
-    a:hover {
-        border-bottom: 2px solid ${props => props.theme.colors.primary};
-        transition: 0.2s ease-in-out;
-      }
+    cursor: pointer;
+    
+    
+    a {
+      padding: 10px;
     }
 
+    .selected-link {
+      border-bottom: 2px solid ${props => props.theme.colors.primary};
+    }
+  }
+
   .links-user {
+    width: 20%;
     display: flex;
     justify-content: space-between;
-    align-items: center;
-    width: 252px;
-    padding: 15px 15px 20px;
+    padding: 10px 20px 10px;
+
+    .cart {
+      position: relative;
+
+      .cart-icon {
+        position: absolute;
+        bottom: 0px;
+        right: 0px;
+        background-color: ${props => props.theme.colors.white};
+        border-radius: 50%;
+        padding: 5px;
+        font-size: 12px;
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
+    }
   }
 
-  @media (max-width: 768px) {
-    width: 100%;
-    padding: 0;
-    height: auto;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    .links-header {
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      width: 100%;
-      a {
-        margin-bottom: 20px;
-      }
-  }
 `;
 
-export default function Header() {
+export default function Header({ cart }: { cart: IProduct[] }) {
   return (
     <HeaderStyled>
       <Image className="logo" width="150px" height="150px" src="/wine-logo.png" />
       <section className="links-header">
-        <a>Clube</a>
-        <a className="selected-link">Loja</a>
-        <a>Produtores</a>
-        <a>Ofertas</a>
-        <a>Eventos</a>
+        <a>
+          Clube
+        </a>
+        <a className="selected-link">
+          Loja
+        </a>
+        <a>
+          Produtores
+        </a>
+        <a>
+          Ofertas
+        </a>
+        <a>
+          Eventos
+        </a>
       </section>
       <section className="links-user">
-        <a><Image width="56px" height="56px" src="/search-interface-symbol.png"/></a>
-        <a><Image width="56px" height="56px" src="/user.png"/></a>
-        <a><Image width="56px" height="56px" src="/winebox.png"/></a>
+        <a>
+          <Image width="56px" height="56px" src="/search-interface-symbol.png" />
+        </a>
+        <a>
+          <Image width="56px" height="56px" src="/user.png" />
+        </a>
+        <a className="cart">
+          <Image width="56px" height="56px" src="/winebox.png" />
+          <span className="cart-icon">
+            {cart.length}
+          </span>
+        </a>
       </section>
     </HeaderStyled>
   );
